73bea6fddb7ab6676a6c843164d70894145c42df
Silvao <silvioquintana1@hotmail.com>
2022-05-20 17:36:40 -0300

    uaiBanck

--- app/Views/Home.php
+++ app/Views/Home.php
@@ -5,11 +5,47 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
+    <link rel="stylesheet" href="/css/global.css">
+    <link rel="stylesheet" href="/css/home.css">
 </head>
-<body>
+<body style='background-color:#A060DE; margin:50px'>
+    <p class='uaiBank'>UaiBank</p>
+    <a class='logout'>Logout</a>
+    <div class='grid'>
+    <div class='BigContainer'>
+    <p>Perfil</p>
+
+    </div>
+    <div class='SmallContainer'>
+    <p>Perfil</p>
+        
+    </div>
+    <div class='SmallContainer2'>
+        
+    </div>
+    <p>Perfil</p>
+    <div class='Container'>
+
+    </div>
+    <div class='Container2'>
+        
+    </div>
+    <div class='Container3'>
+        
+    </div>
+    <div class='SmallContainer3'>
+
+    </div>
+    <div class='SmallContainer4'>
+        
+    </div>
+    <div class='SmallContainer5'>
+        
+    </div>
+    </div>
     <?php
-        $session = session();
-        var_dump($_SESSION);
+  //      $session = session();
+ //       var_dump($_SESSION);
     ?>
 </body>
 </html>
