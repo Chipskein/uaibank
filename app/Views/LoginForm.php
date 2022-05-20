cb91e768ae9369e6fa267479ecdf7beabf72cfee
Silvao <silvioquintana1@hotmail.com>
2022-05-20 15:54:26 -0300

    eh o silvas

--- app/Views/LoginForm.php
+++ app/Views/LoginForm.php
@@ -5,12 +5,24 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>UaiBank</title>
+    <link rel="stylesheet" href="/css/login.css">
+    <link rel="stylesheet" href="/css/global.css">
 </head>
 <body>
-    <form action="/users/login" method="post">
-        <input type="text" placeholder="username" name="username">
-        <input type="password" placeholder="password" name="password">
-        <input type="submit" value="Sign up">
+    <div class='image'></div>
+    <div class='Screen'>
+    <p class='uaiBank'>UaiBank</p>
+    <form class='form' action="/users/login" method="post">
+        <h1 class='logar'>Entrar agora<h1>
+            <p class='inputTitle'>Username</p>
+            <input class='input' type="text" placeholder="Digite seu username" name="username">
+            <p class='inputTitle'>Senha</p>
+            <input class='input' type="password" placeholder="Digite sua senha" name="password">
+            <div class='submitSpace'>
+            <input class='submit' type="submit" value="Logar">
+            <a href='/users/register' class='submitInverted'>Criar Conta</a>
+            </div>
     </form>
+    </div>
 </body>
 </html>
