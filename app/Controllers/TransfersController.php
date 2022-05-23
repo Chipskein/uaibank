f2ae2aed4c80392726791735389055e1b7efa083
Chipskein <abfn0905@gmail.com>
2022-05-23 12:57:17 -0300

    fix:empty input

--- app/Controllers/TransfersController.php
+++ app/Controllers/TransfersController.php
@@ -6,6 +6,7 @@ use App\Controllers\BaseController;
 use App\Models\AccountsModel;
 use App\Models\TransfersModel;
 
+
 class TransfersController extends BaseController
 {
     public function createTransfer()
@@ -13,25 +14,31 @@ class TransfersController extends BaseController
         $toAccId=$this->request->getVar('to');
         $fromAccId=$this->request->getVar('from');
         $transferValue=$this->request->getVar('value');
-        $transferType='normal';
-        $transferDesc='normal transfer';
-        $accModel=new AccountsModel();
-        $verifyBalance=$accModel->verifyBalanceSubstractionFromAccount($fromAccId,$transferValue);
-        $verifyToAccountIsCurrent=$accModel->AccountIsCurrentType($toAccId);
-        if($verifyBalance&&$verifyToAccountIsCurrent){
-            $transferModel=new TransfersModel();
-            $transferId=$transferModel->makeTransferTo($fromAccId,$toAccId,$transferType,$transferDesc,$transferValue);
-            if($transferId){
-                $accModel->removeFromAccount($fromAccId,$transferValue);
-                $accModel->addToAccount($toAccId,$transferValue);
-                $this->session->setFlashdata('sucess','Transação finalizada');
-                return redirect()->to(base_url('/users/'));
+        
+        if(!empty($toAccId)&&!empty($fromAccId)&&!empty($transferValue)){
+            $transferType='normal';
+            $transferDesc='normal transfer';
+            $accModel=new AccountsModel();
+            $verifyBalance=$accModel->verifyBalanceSubstractionFromAccount($fromAccId,$transferValue);
+            $verifyToAccountIsCurrent=$accModel->AccountIsCurrentType($toAccId);
+            if($verifyBalance&&$verifyToAccountIsCurrent){
+                $transferModel=new TransfersModel();
+                $transferId=$transferModel->makeTransferTo($fromAccId,$toAccId,$transferType,$transferDesc,$transferValue);
+                if($transferId){
+                    $accModel->removeFromAccount($fromAccId,$transferValue);
+                    $accModel->addToAccount($toAccId,$transferValue);
+                    $this->session->setFlashdata('sucess','Transação finalizada');
+                    return redirect()->to(base_url('/users/'));
+                } else{
+                    $this->session->setFlashdata('error','Transação não finalizada');
+                    return redirect()->to(base_url('/users/'));
+                }
             } else{
-                $this->session->setFlashdata('error','Transação não finalizada');
+                $this->session->setFlashdata('error','Saldo Insuficiente ou Conta inválidade');
                 return redirect()->to(base_url('/users/'));
             }
         } else{
-            $this->session->setFlashdata('error','Saldo Insuficiente ou Conta inválidade');
+            $this->session->setFlashdata('error','Valores inválidos');
             return redirect()->to(base_url('/users/'));
         }
     }
